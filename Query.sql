f8b1c8266e0bdb0fd8c0d7e9ab0f3422f5a1a65b
Tanisha2304 <87810122+Tanisha2304@users.noreply.github.com>
2023-07-26 20:55:22 +1000

    Instagram Clone.sql

--- Query.sql
+++ Query.sql
@@ -1,3 +1,37 @@
-INSERT INTO Query (Field, Type, Null, Key, Default, Extra) VALUES ('id', 'int', 'NO', 'PRI', NULL, 'auto_increment');
-INSERT INTO Query (Field, Type, Null, Key, Default, Extra) VALUES ('username', 'varchar(255)', 'NO', 'UNI', NULL, '');
-INSERT INTO Query (Field, Type, Null, Key, Default, Extra) VALUES ('created_at', 'timestamp', 'YES', '', 'CURRENT_TIMESTAMP', 'DEFAULT_GENERATED');
+CREATE TABLE users (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+CREATE TABLE photos (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+CREATE TABLE comments (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  comment_text VARCHAR(255) NOT NULL user_id INTEGER NOT NULL,
+  photo_id INTEGER NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (photo_id) REFERENCES photos(id)
+);
+CREATE TABLE likes (
+  user_id INTEGER NOT NULL,
+  photo_id INTEGER NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (photo_id) REFERENCES photos(id),
+  PRIMARY KEY (user_id, photo_id)
+);
+CREATE TABLE follows (
+  follower_id INTEGER NOT NULL,
+  followee_id INTEGER NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY (follower_id) REFERENCES users(id),
+  FOREIGN KEY (followee_id) REFERENCES users(id),
+  PRIMARY KEY (follower_id, followee_id)
+);
+
