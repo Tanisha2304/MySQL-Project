2c8828e1929c0da23019dd2e39e4a252664685b3
Tanisha2304 <87810122+Tanisha2304@users.noreply.github.com>
2023-07-26 20:54:10 +1000

    Add files via upload

---
+++ Query.sql
@@ -0,0 +1,3 @@
+INSERT INTO Query (Field, Type, Null, Key, Default, Extra) VALUES ('id', 'int', 'NO', 'PRI', NULL, 'auto_increment');
+INSERT INTO Query (Field, Type, Null, Key, Default, Extra) VALUES ('username', 'varchar(255)', 'NO', 'UNI', NULL, '');
+INSERT INTO Query (Field, Type, Null, Key, Default, Extra) VALUES ('created_at', 'timestamp', 'YES', '', 'CURRENT_TIMESTAMP', 'DEFAULT_GENERATED');
